91e36517b9a43074b4ca6868477b7431a3811548
KaneKopetski <KaneKopetski@users.noreply.github.com>
2019-10-21 10:21:41 -0400

    Finished quiz. All tests pass.

--- LoopFun.java
+++ LoopFun.java
@@ -1,42 +1,66 @@
- 
 
 public class LoopFun
 {
 
-      /**
-       * Given a number, return the factorial of that number.
-       * For example, given 5, the factorial is 5 x 4 x 3 x 2 x 1 which should return 120.
-       * @param number
-       * @return the factorial of the number
-       */
-      public Integer factorial(Integer number){
-          return null;
-      }
+    /**
+     * Given a number, return the factorial of that number.
+     * For example, given 5, the factorial is 5 x 4 x 3 x 2 x 1 which should return 120.
+     * @param number
+     * @return the factorial of the number
+     */
+    public Integer factorial(Integer number) {
+        int fact = 1;
+        for(int i = 1; i <= number; i++)
+        {
+            fact = fact * i;
+        }
+        return fact;
+    }
 
-      /**
-       * Given a phrase, get the acronym of that phrase. Acronym is the combination of
-       * the first character of each word in upper case.
-       * For example, given "Ruby on Rails", this method will return "ROR"
-       * @param phrase
-       * @return Upper case string of the first letter of each word
-       */
-      public String acronym(String phrase) {
-          return null;
-      }
+    /**
+     * Given a phrase, get the acronym of that phrase. Acronym is the combination of
+     * the first character of each word in upper case.
+     * For example, given "Ruby on Rails", this method will return "ROR"
+     * @param phrase
+     * @return Upper case string of the first letter of each word
+     */
+    public String acronym(String phrase) {
+        String acronym = "";
+        {
+        acronym += phrase.toUpperCase().charAt(0);
 
-      /**
-       * To prevent anyone from reading our messages, we can encrypt it so it will only be readable by its
-       * intended audience. This method encrypt the message by shifting the letter by 3 characters. If the character is
-       * at the end of the alphabet, it will wraps around.
-       * For example:
-       *  'a' => 'd'
-       *  'w' => 'z'
-       *  'x' => 'a'
-       *  'y' => 'b'
-       * @param word
-       * @return the encrypted string by shifting each character by three character
-       */
-      public String encrypt(String word) {
-          return null;
-      }
+        for (int i = 1; i <= phrase.length() - 1; i++) {
+            if (phrase.charAt(i - 1) == ' ' || phrase.charAt(i - 1) == '-') {
+                acronym += phrase.toUpperCase().charAt(i);
+            }
+        }
+
+        return acronym;
+    }
+}
+
+    /**
+     * To prevent anyone from reading our messages, we can encrypt it so it will only be readable by its
+     * intended audience. This method encrypt the message by shifting the letter by 3 characters. If the character is
+     * at the end of the alphabet, it will wraps around.
+     * For example:
+     *  'a' => 'd'
+     *  'w' => 'z'
+     *  'x' => 'a'
+     *  'y' => 'b'
+     * @param word
+     * @return the encrypted string by shifting each character by three character
+     */
+    public String encrypt(String word) {
+        String s = "";
+    int len = word.length();
+    for(int x = 0; x < len; x++){
+        char c = (char)(word.charAt(x) + 3);
+        if (c > 'z')
+            s += (char)(word.charAt(x) - (26-3));
+        else
+            s += (char)(word.charAt(x) + 3);
+    }
+    return s;
+    } 
 }
